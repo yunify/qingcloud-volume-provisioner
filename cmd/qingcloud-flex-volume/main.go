fc8a4c11fdcdea705d238f6398d5db85211cae99
jolestar <jolestar@gmail.com>
2017-08-16 11:39:31 +0800

    refactor flex driver error handler.

--- cmd/qingcloud-flex-volume/main.go
+++ cmd/qingcloud-flex-volume/main.go
@@ -4,58 +4,47 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/golang/glog"
+	qclogger "github.com/yunify/qingcloud-sdk-go/logger"
 	"github.com/yunify/qingcloud-volume-provisioner/pkg/volume/flex"
 	"github.com/yunify/qingcloud-volume-provisioner/pkg/volume/qingcloud"
-	qclogger "github.com/yunify/qingcloud-sdk-go/logger"
 	"os"
 	"path"
-	"path/filepath"
-	"github.com/golang/glog"
 )
 
 const (
-	DriverDir  = "/usr/libexec/kubernetes/kubelet-plugins/volume/exec/"
-	LogDir = "/var/log/qingcloud-flex-volume"
+	DriverDir = "/usr/libexec/kubernetes/kubelet-plugins/volume/exec/"
+	LogDir    = "/var/log/qingcloud-flex-volume"
 )
 
-// fatalf is a convenient method that outputs error in flex volume plugin style
-// and quits
-func fatalf(msg string, args ...interface{}) {
-	err := flex.VolumeResult{
-		Message: fmt.Sprintf(msg, args...),
-		Status:  "Failure",
-	}
-	fmt.Printf(err.ToJson())
-	os.Exit(1)
-}
-
-// printResult is a convenient method for printing result of volume operation
-func printResult(result flex.VolumeResult) {
+// printResult is a convenient method for printing result of volume operation, and return exit code.
+func printResult(result flex.VolumeResult) int {
 	fmt.Printf(result.ToJson())
-	glog.Infof("Response: %#v", result.ToJson())
 	if result.Status == "Success" {
-		os.Exit(0)
+		glog.Infof("Response: %#v", result.ToJson())
+		return 0
 	}
-	os.Exit(1)
+	glog.Errorf("Error: %#v", result.Error())
+	return 1
 }
 
 // ensureVolumeOptions decodes json or die
 func ensureVolumeOptions(v string) (vo flex.VolumeOptions) {
 	err := json.Unmarshal([]byte(v), &vo)
 	if err != nil {
-		fatalf("Invalid json options: %s", v)
+		panic(fmt.Errorf("Invalid json options: %s", v))
 	}
 	return
 }
 
-func installDriver(){
+func installDriver(driverDir string) {
 	ex, err := os.Executable()
 	if err != nil {
 		panic(err)
 	}
 	vendor, driver := path.Split(qingcloud.FlexDriverName)
 	vendor = path.Clean(vendor)
-	driverTargetDir := path.Join(DriverDir, fmt.Sprintf("%s~%s", vendor, driver))
+	driverTargetDir := path.Join(driverDir, fmt.Sprintf("%s~%s", vendor, driver))
 	driverTargetFile := path.Join(driverTargetDir, driver)
 	fmt.Printf("Install driver to %s \n", driverTargetFile)
 	err = os.MkdirAll(driverTargetDir, 0644)
@@ -73,97 +62,116 @@ func installDriver(){
 	}
 }
 
-func main() {
-	install := flag.Bool("install", false, fmt.Sprintf("Install %s to %s", qingcloud.FlexDriverName, DriverDir))
+// GlogWriter serves as a bridge between the standard log package and the glog package.
+type GlogWriter struct{}
 
-	// Prepare logs
-	err := os.MkdirAll(LogDir, 0750)
-	if err != nil {
-		panic(fmt.Sprintf("mkdir %s err: %s", LogDir, err.Error()))
-	}
+// Write implements the io.Writer interface.
+func (writer *GlogWriter) Write(data []byte) (n int, err error) {
+	glog.Info(string(data))
+	return len(data), nil
+}
 
-	flag.Set("logtostderr", "false")
-	flag.Set("alsologtostderr", "false")
-	flag.Set("log_dir", LogDir)
-	flag.Set("stderrthreshold", "FATAL")
-	flag.Set("v", "4")
-	flag.Parse()
-	defer glog.Flush()
-
-	glog.Infof("Call %s driver, args: %#v", qingcloud.FlexDriverName,  flag.Args())
-
-	qcSDKLogFile := filepath.Join(LogDir, "qingcloud_sdk.log")
-	f, err := os.OpenFile(qcSDKLogFile, os.O_WRONLY | os.O_CREATE, 0755)
-	if err != nil {
-		fatalf("Error open log file: %s", qcSDKLogFile)
-	}
-	defer f.Close()
-
-	qclogger.SetOutput(f)
-
-	if *install {
-		flag.VisitAll(func(f *flag.Flag) {
-			glog.Infof("Flag: %s=%s", f.Name, f.Value)
-		})
-		installDriver()
-		return
-	}
-
+func handler(op string, args []string) flex.VolumeResult {
 	volumePlugin, err := qingcloud.NewFlexVolumePlugin()
 
 	if err != nil {
-		fatalf("Error init FlexVolumePlugin")
-	}
-
-	args := flag.Args()
-	if len(args) == 0 {
-		fatalf("Usage: %s init|attach|detach|mountdevice|unmountdevice|waitforattach|getvolumename|isattached", os.Args[0])
+		return flex.NewVolumeError("Error init FlexVolumePlugin")
 	}
 
 	var ret flex.VolumeResult
-	op := args[0]
-	args = args[1:]
+
 	switch op {
 	case "init":
 		ret = volumePlugin.Init()
 	case "attach":
 		if len(args) < 2 {
-			fatalf("attach requires options in json format and a node name")
+			return flex.NewVolumeError("attach requires options in json format and a node name")
 		}
 		ret = volumePlugin.Attach(ensureVolumeOptions(args[0]), args[1])
 	case "isattached":
 		if len(args) < 2 {
-			fatalf("isattached requires options in json format and a node name")
+			return flex.NewVolumeError("isattached requires options in json format and a node name")
 		}
 		ret = volumePlugin.Attach(ensureVolumeOptions(args[0]), args[1])
 	case "detach":
 		if len(args) < 2 {
-			fatalf("detach requires a device path and a node name")
+			return flex.NewVolumeError("detach requires a device path and a node name")
 		}
 		ret = volumePlugin.Detach(args[0], args[1])
 	case "mountdevice":
 		if len(args) < 3 {
-			fatalf("mountdevice requires a mount path, a device path and mount options")
+			return flex.NewVolumeError("mountdevice requires a mount path, a device path and mount options")
 		}
 		ret = volumePlugin.MountDevice(args[0], args[1], ensureVolumeOptions(args[2]))
 	case "unmountdevice":
 		if len(args) < 1 {
-			fatalf("unmountdevice requires a mount path")
+			return flex.NewVolumeError("unmountdevice requires a mount path")
 		}
 		ret = volumePlugin.UnmountDevice(args[0])
 	case "waitforattach":
 		if len(args) < 2 {
-			fatalf("waitforattach requires a device path and options in json format")
+			return flex.NewVolumeError("waitforattach requires a device path and options in json format")
 		}
 		ret = volumePlugin.WaitForAttach(args[0], ensureVolumeOptions(args[1]))
 	case "getvolumename":
 		if len(args) < 1 {
-			fatalf("getvolumename requires options in json format")
+			return flex.NewVolumeError("getvolumename requires options in json format")
 		}
 		ret = volumePlugin.GetVolumeName(ensureVolumeOptions(args[0]))
 	default:
 		ret = flex.NewVolumeNotSupported(op)
 	}
+	return ret
+}
 
-	printResult(ret)
+func mainFunc() int {
+
+	var driverDir string = DriverDir
+	install := flag.Bool("install", false, fmt.Sprintf("Install %s to %s", qingcloud.FlexDriverName, DriverDir))
+	flag.StringVar(&driverDir, "driver_dir", DriverDir, "Driver dir to install.")
+
+	logDir := os.Getenv("LOG_DIR")
+	if logDir == "" {
+		logDir = LogDir
+	}
+	// Prepare logs
+	err := os.MkdirAll(logDir, 0750)
+	if err != nil {
+		panic(fmt.Sprintf("mkdir %s err: %s", logDir, err.Error()))
+	}
+
+	flag.Set("logtostderr", "false")
+	flag.Set("alsologtostderr", "false")
+	flag.Set("log_dir", logDir)
+	flag.Set("stderrthreshold", "FATAL")
+	flag.Set("v", "4")
+	flag.Parse()
+	glog.CopyStandardLogTo("INFO")
+	defer glog.Flush()
+
+	glog.Infof("Call %s driver, args: %#v", qingcloud.FlexDriverName, flag.Args())
+
+	qclogger.SetOutput(&GlogWriter{})
+
+	if *install {
+		flag.VisitAll(func(f *flag.Flag) {
+			glog.Infof("Flag: %s=%s", f.Name, f.Value)
+		})
+		installDriver(driverDir)
+		return 0
+	}
+	var ret flex.VolumeResult
+	args := flag.Args()
+	if len(args) == 0 {
+		ret = flex.NewVolumeError("Usage: %s init|attach|detach|mountdevice|unmountdevice|waitforattach|getvolumename|isattached", os.Args[0])
+	} else {
+		op := args[0]
+		args = args[1:]
+		ret = handler(op, args)
+	}
+	return printResult(ret)
+}
+
+func main() {
+	os.Exit(mainFunc())
 }
