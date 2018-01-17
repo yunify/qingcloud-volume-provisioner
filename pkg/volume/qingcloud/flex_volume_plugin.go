d3fde27d64a0c779e3d95aa716c58b52ec778c12
Calvin Yu <calvinyu@yunify.com>
2018-01-17 15:27:02 +0800

    clean code

--- pkg/volume/qingcloud/flex_volume_plugin.go
+++ pkg/volume/qingcloud/flex_volume_plugin.go
@@ -1,14 +1,15 @@
 package qingcloud
 
 import (
+	"os"
+	"strings"
+	"time"
+
 	"github.com/golang/glog"
 	"github.com/yunify/qingcloud-volume-provisioner/pkg/volume/flex"
 	"k8s.io/kubernetes/pkg/util/exec"
 	"k8s.io/kubernetes/pkg/util/mount"
 	volumeutil "k8s.io/kubernetes/pkg/volume/util"
-	"os"
-	"strings"
-	"time"
 )
 
 const (
@@ -30,6 +31,7 @@ type flexVolumePlugin struct {
 }
 
 func NewFlexVolumePlugin() (flex.VolumePlugin, error) {
+	glog.V(4).Infof("NewFlexVolumePlugin")
 	manager, err := newVolumeManager(DefaultQingCloudConfigPath)
 	if err != nil {
 		return nil, err
@@ -38,10 +40,12 @@ func NewFlexVolumePlugin() (flex.VolumePlugin, error) {
 }
 
 func (*flexVolumePlugin) Init() flex.VolumeResult {
+	glog.V(4).Infof("Init")
 	return flex.NewVolumeSuccess()
 }
 
 func (p *flexVolumePlugin) Attach(options flex.VolumeOptions, node string) flex.VolumeResult {
+	glog.V(4).Infof("Attach")
 	volumeID, _ := options[OptionVolumeID].(string)
 	pvOrVolumeName, _ := options[OptionPVorVolumeName].(string)
 	// flexVolumeDriver GetVolumeName is not yet supported,  so PVorVolumeName is pvName, and store pvName to volumeName
@@ -53,7 +57,8 @@ func (p *flexVolumePlugin) Attach(options flex.VolumeOptions, node string) flex.
 	}
 	// VolumeManager.AttachVolume checks if disk is already attached to node and
 	// succeeds in that case, so no need to do that separately.
-	devicePath, err := p.manager.AttachVolume(volumeID, node)
+	_, err := p.manager.AttachVolume(volumeID, node)
+
 	if err != nil {
 		//ignore already attached error
 		if !strings.Contains(err.Error(), "have been already attached to instance") {
@@ -61,10 +66,12 @@ func (p *flexVolumePlugin) Attach(options flex.VolumeOptions, node string) flex.
 			return flex.NewVolumeError("Error attaching volume %q to node %s: %+v", volumeID, node, err)
 		}
 	}
-	return flex.NewVolumeSuccess().WithDevicePath(devicePath)
+
+	return flex.NewVolumeSuccess().WithDevicePath(volumeID)
 }
 
 func (p *flexVolumePlugin) Detach(pvOrVolumeName string, node string) flex.VolumeResult {
+	glog.V(4).Infof("Detach volume %v from node %v", pvOrVolumeName, node)
 	var volumeID string
 	var err error
 	if !isVolumeID(pvOrVolumeName) {
@@ -82,11 +89,24 @@ func (p *flexVolumePlugin) Detach(pvOrVolumeName string, node string) flex.Volum
 	return flex.NewVolumeSuccess()
 }
 
-func (*flexVolumePlugin) MountDevice(dir, device string, options flex.VolumeOptions) flex.VolumeResult {
+/* To solve device path and volume mapping issue(https://github.com/kubernetes/kubernetes/issues/55314),
+   this plugin use volumeID to identify device path by calling qingcloud volume service.
+   as volumeID could be get from volume options, so just skip the first input parm about device data.
+*/
+func (p *flexVolumePlugin) MountDevice(dir, _ string, options flex.VolumeOptions) flex.VolumeResult {
+	glog.V(4).Infof("MountDevice called, options: [%+v]", options)
+	volumeID, _ := options[OptionVolumeID].(string)
+	deviceOnQingCloud, err := p.manager.GetDeviceByVolumeID(volumeID)
+	if err != nil {
+		return flex.NewVolumeError("Error locate device by volumeID (%q): %v", volumeID, err)
+	}
+	glog.V(4).Infof("MountDevice volume %v device %v", volumeID, deviceOnQingCloud)
+
 	fstype, _ := options[OptionFSType].(string)
 	if fstype == "" {
 		fstype = DefaultFSType
 	}
+	glog.V(4).Infof("MountDevice fsType is %v", fstype)
 	readwrite, _ := options[OptionReadWrite].(string)
 	flagstr, _ := options["flags"].(string)
 	flags := []string{}
@@ -102,17 +122,18 @@ func (*flexVolumePlugin) MountDevice(dir, device string, options flex.VolumeOpti
 			return flex.NewVolumeError(err.Error())
 		}
 	}
-
+	glog.V(4).Infof("MountDevice device %s dir %s", deviceOnQingCloud, dir)
 	volumeMounter := &mount.SafeFormatAndMount{Interface: mount.New(""), Runner: exec.New()}
-	err := volumeMounter.FormatAndMount(device, dir, fstype, flags)
+	err = volumeMounter.FormatAndMount(deviceOnQingCloud, dir, fstype, flags)
 	if err != nil {
 		os.Remove(dir)
-		return flex.NewVolumeError("FormatAndMount device (%s) dir (%s) ", device, dir, err.Error())
+		return flex.NewVolumeError("FormatAndMount device (%s) dir (%s) ", deviceOnQingCloud, dir, err.Error())
 	}
 	return flex.NewVolumeSuccess()
 }
 
 func (*flexVolumePlugin) UnmountDevice(dir string) flex.VolumeResult {
+	glog.V(4).Infof("UnmountDevice %v", dir)
 	mounter := &mount.SafeFormatAndMount{Interface: mount.New(""), Runner: exec.New()}
 	err := mounter.Unmount(dir)
 	if err != nil {
@@ -121,12 +142,20 @@ func (*flexVolumePlugin) UnmountDevice(dir string) flex.VolumeResult {
 	return flex.NewVolumeSuccess()
 }
 
-func (*flexVolumePlugin) WaitForAttach(device string, options flex.VolumeOptions) flex.VolumeResult {
+/* To solve device path and volume mapping issue(https://github.com/kubernetes/kubernetes/issues/55314),
+   this plugin use volumeID to identify device path by calling qingcloud volume service.
+   as volumeID could be get from volume options, so just skip the first input parm about device data.
+*/
+func (p *flexVolumePlugin) WaitForAttach(_ string, options flex.VolumeOptions) flex.VolumeResult {
 	volumeID, _ := options[OptionVolumeID].(string)
-
-	if device == "" {
-		return flex.NewVolumeError("WaitForAttach failed for  Volume %q: device is empty.", volumeID)
+	deviceOnQingCloud, err := p.manager.GetDeviceByVolumeID(volumeID)
+	if err != nil {
+		return flex.NewVolumeError("Error locate device by volumeID (%q): %v", volumeID, err)
 	}
+	glog.V(4).Infof("WaitForAttach volume %v device %v", volumeID, deviceOnQingCloud)
+	//if device == "" {
+	//	return flex.NewVolumeError("WaitForAttach failed for  Volume %q: device is empty.", volumeID)
+	//}
 
 	ticker := time.NewTicker(checkSleepDuration)
 	defer ticker.Stop()
@@ -135,20 +164,22 @@ func (*flexVolumePlugin) WaitForAttach(device string, options flex.VolumeOptions
 		select {
 		case <-ticker.C:
 			glog.V(4).Infof("Checking  volume %q is attached.", volumeID)
-			exists, err := volumeutil.PathExists(device)
+
+			exists, err := volumeutil.PathExists(deviceOnQingCloud)
 			if err != nil {
 				// Log error, if any, and continue checking periodically.
 				glog.Errorf("Error verifying  volume (%q) is attached: %v", volumeID, err)
 			} else if exists {
 				// A device path has successfully been created for the PD
 				glog.Infof("Successfully found attached  volume %q.", volumeID)
-				return flex.NewVolumeSuccess().WithDevicePath(device)
+				return flex.NewVolumeSuccess().WithDevicePath(volumeID)
 			}
 		}
 	}
 }
 
 func (*flexVolumePlugin) GetVolumeName(options flex.VolumeOptions) flex.VolumeResult {
+	glog.V(4).Infof("GetVolumeName")
 	//TODO to implements this method when k8s 1.8 fix bug: https://github.com/kubernetes/kubernetes/issues/44737
 	//and https://github.com/kubernetes/kubernetes/blob/f39c6087c2b2b473c37618d9cd054d918be0f77a/pkg/volume/flexvolume/plugin.go#L123
 	// implements getvolumename call.
@@ -157,6 +188,7 @@ func (*flexVolumePlugin) GetVolumeName(options flex.VolumeOptions) flex.VolumeRe
 }
 
 func (p *flexVolumePlugin) IsAttached(options flex.VolumeOptions, node string) flex.VolumeResult {
+	glog.V(4).Infof("IsAttached")
 	volumeID, _ := options[OptionVolumeID].(string)
 	r, err := p.manager.VolumeIsAttached(volumeID, node)
 
